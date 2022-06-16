b6a85d1a105ac38e9aec111d9a2eb9b087611820
James Lawrence <jljatone@gmail.com>
2022-06-16 12:09:24 -0400

    allow output to file

--- cmd/vaultenv/main.go
+++ cmd/vaultenv/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"io"
 	"log"
 	"os"
 
@@ -11,15 +13,17 @@ import (
 func main() {
 	var (
 		err     error
+		path    string
 		secrets []string
 		clean   bool
 		v       vaultenv.Vault
+		b       = bytes.NewBufferString("")
 	)
 
 	app := kingpin.New("vaultenv", "converts vault secrets to environment variables")
 	app.Arg("secrets", "paths to secrets being read").StringsVar(&secrets)
 	app.Flag("clean", "clear the environment before merging secrets").Default("false").BoolVar(&clean)
-
+	app.Flag("output", "set the destination file to output; defaults to stdout").Short('o').StringVar(&path)
 	if _, err = app.Parse(os.Args[1:]); err != nil {
 		log.Fatalln(err)
 		return
@@ -41,9 +45,23 @@ func main() {
 	}
 
 	for _, env := range os.Environ() {
-		if _, err = os.Stdout.WriteString(env); err != nil {
+		if _, err = b.WriteString(env); err != nil {
 			log.Fatalln(err)
 		}
-		os.Stdout.WriteString("\n")
+
+		b.WriteString("\n")
+	}
+
+	var out io.Writer = os.Stdout
+	if len(path) > 0 {
+		if dst, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_RDWR, 0600); err != nil {
+			log.Fatalln(err)
+		} else {
+			out = dst
+		}
+	}
+
+	if _, err = io.Copy(out, b); err != nil {
+		log.Fatalln(err)
 	}
 }
